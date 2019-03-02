266cbe25436b7d9242b31764c3e14a0e996011c7
Istvan Nemeth <nemethistvan.ius@gmail.com>
2019-03-02 11:39:14 +0100

    refactor(main): refactor parsing to different func and defining bools instead of pointers

--- main.go
+++ main.go
@@ -7,25 +7,44 @@ import (
 	"strings"
 )
 
+var pFlag bool
+var hFlag bool
+var gFlag bool
+
 func main() {
-	p := flag.Bool("p", false, "Print what would be the result of the cleaning, but do nothing.")
-	flag.Parse()
-	if *p {
+	parseFlags()
+	if pFlag {
 		printCleanPath()
 		return
 	}
 
-	welcome()
+	if gFlag {
 
+	}
+
+	if hFlag {
+		welcome()
+		return
+	}
+
+	welcome()
+}
+
+func parseFlags() {
+	flag.BoolVar(&hFlag, "h", false, "Prints the help page.")
+	flag.BoolVar(&gFlag, "g", false, "Shows your current PATH.")
+	flag.BoolVar(&pFlag, "p", false, "Print what would be the result of the cleaning, but do nothing.")
+	flag.Parse()
 }
 
 func welcome() {
 	fmt.Println(`
-
 Welcome to cleanpath, a lightweight Go CLI app to clean your path from duplicates!
 	
-Usage: CLI Template [OPTIONS]
+Usage: cleanpath [OPTIONS]
 Options:
+	-h,		Show help page.
+	-g,		Shows your current PATH.
 	-c,		Clean path of duplicates.
 	-p,		Print what would be the result of the cleaning, but do nothing.
 	`)
