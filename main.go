5750e17621bce42d4d243b32e914b05d52c5fc14
Istvan Nemeth <nemethistvan.ius@gmail.com>
2019-03-02 12:28:26 +0100

    feat(getDuplicateNumber): add func and print method to show duplicate number in path

--- main.go
+++ main.go
@@ -7,33 +7,37 @@ import (
 	"strings"
 )
 
-var pFlag bool
 var hFlag bool
 var gFlag bool
+var fFlag bool
+var pFlag bool
 var path = os.Getenv("PATH")
 
 func main() {
 	parseFlags()
-	if gFlag {
-		getCurrentPath()
-		return
-	}
-	if pFlag {
-		printCleanPath()
-		return
-	}
 	if hFlag {
 		welcome()
 		return
 	}
-
+	if gFlag {
+		printCurrentPath()
+		return
+	}
+	if fFlag {
+		fmt.Printf("You have %d duplicates in your path.\n", getDuplicateNumber())
+		return
+	}
+	if pFlag {
+		return
+	}
 	welcome()
 }
 
 func parseFlags() {
 	flag.BoolVar(&hFlag, "h", false, "Prints the help page.")
 	flag.BoolVar(&gFlag, "g", false, "Shows your current PATH.")
-	flag.BoolVar(&pFlag, "p", false, "Print what would be the result of the cleaning, but do nothing.")
+	flag.BoolVar(&fFlag, "f", false, "Prints the amount of duplicates found in your path.")
+	flag.BoolVar(&pFlag, "p", false, "Print what would be the result of the cleaning, but doesn't change path.")
 	flag.Parse()
 }
 
@@ -45,20 +49,46 @@ Usage: cleanpath [OPTIONS]
 Options:
 	-h,		Shows help page.
 	-g,		Shows your current PATH.
+	-f,		Prints the amount of duplicates found in your path.
 	-c,		Cleans path of duplicates.
-	-p,		Prints what would be the result of the cleaning, but do nothing.
+	-p,		Prints what would be the result of the cleaning, but doesn't change path.
 	`)
 }
 
-func getCurrentPath() {
+func printCurrentPath() {
 	fmt.Printf("Your current path is:\n %s\n", path)
 }
 
-func printCleanPath() {
+func getSplitPath() []string {
 	f := func(r rune) bool {
 		return r == rune(':')
 	}
 
-	a := strings.FieldsFunc(path, f)
-	fmt.Println(a)
+	s := strings.FieldsFunc(path, f)
+	return s
+}
+
+func ifContains(slice []string, word string) bool {
+	for _, v := range slice {
+		if v == word {
+			return true
+		}
+	}
+	return false
+}
+
+func getDuplicateNumber() int {
+	s := getSplitPath()
+	var tmp []string
+	var duplicates []string
+
+	for _, v := range s {
+		if ifContains(tmp, v) {
+			duplicates = append(duplicates, v)
+		} else {
+			tmp = append(tmp, v)
+		}
+	}
+
+	return len(duplicates)
 }
